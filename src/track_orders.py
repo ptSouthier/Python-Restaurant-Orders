4909f8968512004ad703e7f567f5be7228b722e6
Southier <patrick.southier@hotmail.com>
2022-03-13 22:53:27 -0300

    feat: implementing the add_new_order function to the TrackOrders class at track_orders

--- src/track_orders.py
+++ src/track_orders.py
@@ -6,7 +6,7 @@ class TrackOrders:
         return len(self.data)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.data.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         pass
